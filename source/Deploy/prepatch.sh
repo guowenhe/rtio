6e1a13edaf189f5724fac7b765e06b84e041fda8
wenhe <guo_heboy@163.com>
2020-06-11 11:45:26 +0800

    update deploy doc

--- source/Deploy/prepatch.sh
+++ source/Deploy/prepatch.sh
@@ -3,7 +3,6 @@
 #mkdir patches/ 
 
 
-
 icegridadmin -u admin -p admin01 --Ice.Default.Locator="Grid-001/Locator:default -h localhost -p 12000" -e "server stop Facilities.IcePatch2"
 icepatch2calc -V ./patches
 echo "##################################################"
@@ -11,7 +10,7 @@ echo "##################################################"
 # read
 icegridadmin -u admin -p admin01 --Ice.Default.Locator="Grid-001/Locator:default -h localhost -p 12000" -e "application patch -f DMS"
 
-#icegridadmin -u admin -p admin01 --Ice.Default.Locator="PayServerGrid/Locator:default -h localhost -p 4062" -e "server patch -f PayServer.Cashier-1"
+#icegridadmin -u admin -p admin01 --Ice.Default.Locator="Grid-001/Locator:default -h localhost -p 12000" -e "server patch -f DMS.DeviceHub-1"
 echo "patching ..."
 
 echo "##################################################"
